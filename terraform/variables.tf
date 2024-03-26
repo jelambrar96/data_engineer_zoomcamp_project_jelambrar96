c9d50183dd03469982c556f921b170af7734332d
jelambrar96 <jelambrar@gmail.com>
2024-03-26 01:26:04 -0500

    jelambrar96_terraform_google_cloud_storage

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable project {
+    description = ""
+    type = string
+}
+
+variable region {
+    default = "us-central1"
+    description = ""
+    type = string
+}
+
+variable zone {
+    default = "us-central1-a"
+    description = ""
+    type = string
+}
+
