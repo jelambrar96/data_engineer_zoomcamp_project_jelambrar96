dece6fd4df1ad7d674135a5ba0f8420856672c28
jelambrar96 <jelambrar@gmail.com>
2024-04-07 11:14:01 -0500

    jelambrar96_cloud_composer

--- composer/dags/dag_01_download_gh_archive_data.py
+++ composer/dags/dag_01_download_gh_archive_data.py
@@ -1,53 +1,41 @@
+# import natives
+import os
+
+# from import natives
 from datetime import timedelta, datetime
 
+# import external dependences
 import requests
 
+# import airflow dependences
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 
 # Google cloud operators
 from airflow.models import Variable
-# from airflow.providers.google.cloud.operators.functions import (
-#     CloudFunctionDeleteFunctionOperator,
-#     CloudFunctionDeployFunctionOperator,
-#     CloudFunctionInvokeFunctionOperator,
-# )
+
+# import google dependences
 import google.auth.transport.requests
 import google.oauth2.id_token
 
 
-
-# initializing the default arguments
-default_args = {
-    'start_date': datetime(2024, 3, 29),
-    'retries': 3,
-    'retry_delay': timedelta(hours=2)
-}
-
+URL_CLOUD_FUNCTION = os.environ.get('DOWNLOAD_GH_DATA_CLOUD_FUNCTION')
+START_DATE_STR = os.environ.get('START_DATE', '2024-04-01')
+start_date = datetime.strftime(START_DATE_STR, "%Y-%m-%d")
 
 function_invoke_cf_download = "download_gh_archive_data"
-gcp_conn_id = "sa-med-ml"
-
-
-# env_vars = Variable.get("TPM_ETA_VARS", deserialize_json=True)
-# project_id = env_vars["GOOGLE_CLOUD_PROJECT_ID"]
-# ocation = env_vars["LOCATION"]
-# project_id = Variable.get("GOOGLE_CLOUD_PROJECT_ID")
-# location = Variable.get("LOCATION")
-
-
-url = "https://us-central1-jelambrar96-zoomcamp-20240331.cloudfunctions.net/jelambrar96-zoomcamp-20240331-download-google-function"
+# gcp_conn_id = "sa-med-ml"
 
 auth_req = google.auth.transport.requests.Request()
-id_token = google.oauth2.id_token.fetch_id_token(auth_req, url)
+id_token = google.oauth2.id_token.fetch_id_token(auth_req, URL_CLOUD_FUNCTION)
 headers = {'content-type': 'application/json', "Authorization": f"Bearer {id_token}"}
 
 
 def function_invoke_cf_download(date, hour):
     payload = {'date': date, 'hour': hour }
     try:
-        response = requests.post(url, json=payload, headers=headers, timeout=60*3)
+        response = requests.post(URL_CLOUD_FUNCTION, json=payload, headers=headers, timeout=60*3)
         if response.status_code == 200:
                 output = "Data downloaded"
         else:
@@ -56,6 +44,13 @@ def function_invoke_cf_download(date, hour):
     except Exception as e:
          return "Error"
 
+
+# initializing the default arguments
+default_args = {
+    'start_date': start_date,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=10)
+}
 
 with DAG(
     dag_id='dag_01_download_gh_archive_data',
@@ -66,20 +61,10 @@ with DAG(
     task_start = DummyOperator(task_id='task_start')
     task_end = DummyOperator(task_id='task_end')
 
-    # task_invoke_cf_download = CloudFunctionInvokeFunctionOperator(
-    #     task_id = "task_invoke_cf_download",
-    #     project_id = project_id,
-    #     location = location,
-    #     gcp_conn_id = gcp_conn_id,
-    #     input_data = {"date": "{{prev_ds}}"},
-    #     function_id = function_invoke_cf_download,
-    #     dag = dag
-    # )
-
     task_invoke_cf_download = PythonOperator(
         task_id="task_invoke_cf_download",
         python_callable=function_invoke_cf_download,
-        op_kwargs={'date': '{{ prev_ds }}', 'hour': '{{ execution_date.hour}}'}
+        op_kwargs={'date': '{{ ds }}', 'hour': '{{ execution_date.hour}}'}
     )
 
     task_start >> task_invoke_cf_download >> task_end
